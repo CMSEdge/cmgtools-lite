a293aabb0896d7d464c89774e1d704cee29585fe
Celia Fernandez Madrazo <celiaafernandez95@gmail.com>
2018-11-23 12:21:48 +0100

    Some changes on variables, modifications to avoid the code to interrupt running

--- TTHAnalysis/macros/prepareEventVariablesFriendTreeEdge.py
+++ TTHAnalysis/macros/prepareEventVariablesFriendTreeEdge.py
@@ -12,7 +12,7 @@ from CMGTools.TTHAnalysis.tools.edgeFriends import *
 
 
 MODULES.append( ('edgeFriends', edgeFriends("Edge",  
-                                lambda lep : _susyEdgeTight(lep),
+                                lambda lep : _susyEdgeLoose(lep),
                                 cleanJet = lambda lep,jet,dr : (jet.pt < 35 and dr < 0.4)) ) )
 
 
